6a4f1704760f8f6d9f3d6b710b602e82b68adc59
Vimpil <vimpil@ya.ru>
2018-09-23 20:43:09 +0300

    all corrects

--- contacts.markdown
+++ contacts.markdown
@@ -2,40 +2,31 @@
 title: Contacts
 date: 2018-09-12 14:59:00 Z
 order: 2
-layout: default
+layout: page
 ---
-
 <div class="two-seps">
-						<div class="need_more">
-							<div class="text">
-								<p>Need more info? Do you have some question or suggestion? Or you want to start a project?
-								Send me a message! I’ll be glad to hear from you!</p>
-							</div>							
-							<div class="form_title">Any questions?</div>
-							<div class="form_wrap">
-								<div class="fields">
-								<form id="contacts"></form>
-								<p>
-									<input placeholder="Name" name="user" form="contacts" id="user_name">
-								</p>
-								<p>
-									<input placeholder="Phone" name="phone" form="contacts">
-								</p>
-								<p>
-									<input placeholder="E-mail" name="e-mail" form="contacts">
-								</p>
-							</div>
-							<div class="send_wrap">
-								<div class="left_decor"></div>
-								<div class="before">
-									<img src="images/contacts_btn_left.png" alt="">
-								</div>
-								<button id="questions">
-								<span>Send</span>
-								</button>
-								<div class="after">
-									<img src="images/contacts_btn_right.png" alt="">
-								</div>
-								<div class="right_decor"></div>
-							</div>
-						</div>
+	<div class="need_more">
+		<div class="text">
+			<p>Need more info? Do you have some question or suggestion? Or you want to start a project?
+			Send me a message! I’ll be glad to hear from you!</p>
+		</div>
+		<div class="form_title">Any questions?</div>
+		<div class="form_wrap">
+			<div class="fields">
+			<form id="contacts"></form>
+			<p>
+				<input placeholder="Name" name="user" form="contacts" id="user_name">
+			</p>
+			<p>
+				<input placeholder="Phone" name="phone" form="contacts" id="phone_number">
+			</p>
+			<p>
+				<input placeholder="E-mail" name="e_mail" form="contacts" id="e_mail">
+			</p>
+		</div>
+		<div class="send_wrap">
+			<button id="questions">
+			<span>Send</span>
+			</button>
+		</div>
+	</div>
--- js/script.js
+++ js/script.js
@@ -1,43 +1,3 @@
-var filterizd = $('.filtr-container').filterizr({    //options object
-});
-
-// Default options
-var options = {   
-	animationDuration: 0.5, // in seconds
-	   filter: 'all', // Initial filter
-	   callbacks: {      
-		onFilteringStart: function() {},
-		      onFilteringEnd: function() {},
-		      onShufflingStart: function() {},
-		      onShufflingEnd: function() {},
-		      onSortingStart: function() {},
-		      onSortingEnd: function() {}   
-	},
-	   controlsSelector: '', // Selector for custom controls
-	   delay: 0, // Transition delay in ms
-	   delayMode: 'progressive', // 'progressive' or 'alternate'
-	   easing: 'ease-out',
-	   filterOutCss: { // Filtering out animation
-		      
-		opacity: 0,
-		      transform: 'scale(0.5)'   
-	},
-	   filterInCss: { // Filtering in animation
-		      
-		opacity: 0,
-		      transform: 'scale(1)'   
-	},
-	   layout: 'sameSize', // See layouts
-	   multifilterLogicalOperator: 'or',
-	   selector: '.filtr-container',
-	   setupControls: true // Should be false if controlsSelector is set 
-}
-// You can override any of these options and then call...
-var filterizd = $('.filtr-container').filterizr(options);
-// If you have already instantiated your Filterizr then call...
-filterizd.filterizr('setOptions', options);
-
-
 $(document).ready(function() {
 
 	$("ul.nav.nav-gallery.filters-filteringModeSingle li").click(function() {
@@ -71,6 +31,53 @@ $(document).ready(function() {
 
 	$(function() {
 		$('nav#menu').mmenu()
+	})
+
+	$(function() {
+
+		$("#menu").mmenu({}, {});
+		$(".mh-head.mm-sticky").mhead({
+			scroll: {
+				hide: 200
+			}
+		});
+		$(".mh-head:not(.mm-sticky)").mhead({
+			scroll: false
+		});
+
+		$('body').on('click',
+			'a[href^="#/"]',
+			function() {
+				alert("Thank you for clicking, but that's a demo link.");
+				return false;
+			}
+		);
 	});
+
+	document.getElementById("questions").onclick = function() {
+		console.log("clicked");
+		var name_input = document.getElementById('user_name').value;
+		var phone_input = document.getElementById('phone_number').value;
+		var e_mail_input = document.getElementById('e_mail').value;
+
+		$.ajax({
+			url: "https://formspree.io/vimpil04@gmail.com",
+			method: "POST",
+			data: {
+				message: "hello!"
+			},
+			dataType: "json"
+		});
+	};
+	// END fancybox load
+
+	//SVG path's hover
+
+	$("a#ysmm").hover({
+		function(){
+		console.log("hover")
+	}
+	});
+	
+	//END SVG path's hover
 });
-// END fancybox load
